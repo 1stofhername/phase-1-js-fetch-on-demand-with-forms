abe7fa418784f473960fa5b3a15da3b6332788c6
Alicia <arrowsrp@yahoo.com>
2022-01-02 20:43:24 -0700

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const init = () => {
   
+  const inputForm = document.querySelector('form');
+
+  inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const input = event.target.children[1].value
+
+      console.log(input)
+  
+
+  fetch(`http://localhost:3000/movies/${input}`)
+  .then(res => res.json())
+  .then(data => {
+      const title = (document.querySelector('#movieTitle'));
+      const summary = (document.querySelector('#movieSummary'));
+
+      title.innerText = data.title;
+      summary.innerText = data.summary;
+
+  })
+})
+
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
